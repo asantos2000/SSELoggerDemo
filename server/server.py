b703bdc1cd688229d90b037338923524699a5442
Anderson Santos <adsantos@gmail.com>
2021-10-03 00:03:27 -0300

    pep format

--- server/server.py
+++ server/server.py
@@ -6,16 +6,17 @@ logs are added to our log file.
 
 from fastapi import FastAPI, Request
 from sse_starlette.sse import EventSourceResponse
-from datetime import datetime 
+from datetime import datetime
 import uvicorn
 from sh import tail
 from fastapi.middleware.cors import CORSMiddleware
-import time 
+import time
 import os
-#create our app instance
+
+# create our app instance
 app = FastAPI()
 
-#add CORS so our web page can connect to our api
+# add CORS so our web page can connect to our api
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -26,8 +27,8 @@ app.add_middleware(
 real_path = os.path.realpath(__file__)
 dir_path = os.path.dirname(real_path)
 LOGFILE = f"{dir_path}/test.log"
-#This async generator will listen to our log file in an infinite while loop (happens in the tail command)
-#Anytime the generator detects a new line in the log file, it will yield it.
+# This async generator will listen to our log file in an infinite while loop (happens in the tail command)
+# Anytime the generator detects a new line in the log file, it will yield it.
 async def logGenerator(request):
     for line in tail("-f", LOGFILE, _iter=True):
         if await request.is_disconnected():
@@ -36,11 +37,13 @@ async def logGenerator(request):
         yield line
         time.sleep(0.5)
 
-#This is our api endpoint. When a client subscribes to this endpoint, they will recieve SSE from our log file
-@app.get('/stream-logs')
+
+# This is our api endpoint. When a client subscribes to this endpoint, they will recieve SSE from our log file
+@app.get("/stream-logs")
 async def runStatus(request: Request):
     event_generator = logGenerator(request)
     return EventSourceResponse(event_generator)
 
-#run the app
+
+# run the app
 uvicorn.run(app, host="0.0.0.0", port=8000, debug=True)
